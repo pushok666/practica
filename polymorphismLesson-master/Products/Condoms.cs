87fd8b24ed60d11b192875438c27639655222d71
bloknet <barinov_tosha@inbox.ru>
2021-03-03 23:13:34 +0300

    polimorfizm praktica

---
+++ polymorphismLesson-master/Products/Condoms.cs
@@ -0,0 +1,23 @@
+﻿namespace SexShop.Products
+{
+    class Condoms : Product
+    {
+        public int Size { get; set; }
+
+        public Condoms(string name, int price, string manufacturer, int size)
+        {
+            Name = name;
+            Manufacturer = manufacturer;
+            Price = price;
+            Size = size;
+        }
+        public override double GetDiscountPrice(User user)
+        {
+            if (Name[0]=='З')
+            {
+                Price -= 30;
+            }
+            return Price;
+        }
+    }
+}
