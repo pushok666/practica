ddc7e9c940309ba71800d48b8284e2d9b738f16d
bloknet <barinov_tosha@inbox.ru>
2021-03-15 15:36:53 +0300

    connect to db

---
+++ EFCore/BaseEF/TestConnect/TestConnect/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace TestConnect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(helloappdbContext db = new helloappdbContext())
+            {
+                var users = db.Users.ToList();
+                foreach (var item in users)
+                {
+                    Console.WriteLine($"{item.Id}.{item.Name} - {item.Age}");
+                }
+            }
+        }
+    }
+}
